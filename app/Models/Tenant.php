a5397022c4d983d49eb8d76f56c1ca5c29a65d22
kutteai <kutteai978@gmail.com>
2025-11-17 08:15:42 -0500

    Fix: Explicitly implement TenantWithDatabase interface
    - Add TenantWithDatabase interface implementation
    - Required for stancl/tenancy CreateDatabase job

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
 
-class Tenant extends BaseTenant
+class Tenant extends BaseTenant implements TenantWithDatabase
 {
     use HasFactory;
 
